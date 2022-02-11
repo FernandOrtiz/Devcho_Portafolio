2e07213ddef62db72971eb5ddea4e69a59a7fcb9
fernandOrtiz <fernando_orm1@outlook.com>
2022-02-10 20:23:10 -0600

    diseño responsivo de la barra de navegacion y el menu desplegable

---
+++ js/main.js
@@ -0,0 +1,58 @@
+//Variables
+//Para la barra de navegacion
+let nav = document.getElementById('nav');
+//Para el menu responsivo
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('menu');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+//barra de navegacion
+function barranav(){
+    let desplazamientoActual = window.pageYOffset;
+    if(desplazamientoActual >= 480){
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        abrir.style.color = 'rgb(0,64,64)';
+    }else{
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
+    }
+}
+
+window.addEventListener('load', function(){
+    barranav();
+});
+
+window.addEventListener('scroll', function(){
+    // console.log(window.pageYOffset);
+    barranav();
+});
+
+//menu responsivo
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+abrir.addEventListener('click', function(){
+    apertura();
+});
+
+window.addEventListener('resize', function(){
+    if(screen.width >= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
